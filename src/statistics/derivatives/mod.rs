15f331efc2961cc54fe6bae939a5e79b1d858a43
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2025-04-05 15:40:40 -0700

    feat(statistics): added second-order time-shifted derivatives
    These are much simpler than first-order time-shifted derivatives.
    Details can be found in Typst document `/docs/derivative.typ`. A
    reference to that document will be added to documentation next commit.

--- src/statistics/derivatives/mod.rs
+++ src/statistics/derivatives/mod.rs
@@ -239,19 +239,15 @@ pub fn nth_order<T: Float, F: Float>(order: NonZeroU32, list: &[(T, F)]) -> Box<
 ///
 /// # Errors
 ///
-/// Returns [`None`] if `index`, `index - 1`, or `index + 1` is out of bounds in `list`.
+/// Returns [`None`] if `index - 1`, or `index + 1` is out of bounds in `list`.
 #[must_use]
 fn derivative_time_shifted<T: Float, F: Float>(index: usize, list: &[(T, F)]) -> Option<(T, f64)> {
-    if index == 0 {
-        return None;
-    }
-
     let get = |index: usize| {
         let (t, f) = list.get(index)?;
         Some((t.get(), f.get()))
     };
 
-    let (independent_1, dependent_1) = get(index - 1)?;
+    let (independent_1, dependent_1) = get(index.checked_sub(1)?)?;
     let (independent_2, dependent_2) = get(index)?;
     let (independent_3, dependent_3) = get(index + 1)?;
 
@@ -384,6 +380,180 @@ pub fn first_order_time_shifted<T: Float, F: Float>(list: &[(T, F)]) -> Box<[(T,
     for i in 1..(list.len() - 1) {
         derivative.push(
             derivative_time_shifted(i, list).expect("`1 < i < list.len() - 1`, this is safe"),
+        );
+    }
+
+    derivative.into_boxed_slice()
+}
+
+/// Calculates the numerical second derivative of `F` with respect to `T` at `index` using
+/// time-shifted data points.
+///
+/// This recognizes that traditional "rise over run" derivatives calculate the average derivative,
+/// at the center of a time interval. It is a normal [`central_difference_derivative`] of the
+/// [`first_order_time_shifted`] derivative, but running from the midpoint of `t_1` to `t_2`
+/// and the midpoint of `t_2` to `t_3`, instead of from `t_1` to `t_3`.
+///
+/// Here's the math written out as a Typst expression, calculating acceleration from change in
+/// velocity over time:
+///
+/// ```typst
+/// $$$
+///     a_2 &= 2 * (
+///         v_("avg", 23)
+///         - v_("avg", 12)
+///     ) / (Delta t_13)\
+///
+///     a_2 &= 2 * (
+///         (x_3 - x_2) / (t_3 - t_2)
+///         - (x_2 - x_1) / (t_2 - t_1)
+///     ) / (t_3 - t_1)\
+/// $$$
+/// ```
+///
+// TODO: refer to the Typst document instead?
+/// For more, see William Leonard's "The Dangers of Automated Data Analysis," pub. _The Physics
+/// Teacher,_ vol. 35, April 1996, p. 220.
+///
+/// # Errors
+///
+/// Returns [`None`] if `index - 1` or `index + 1` is out of bounds in `list`.
+#[must_use]
+fn second_derivative_time_shifted<T: Float, F: Float>(
+    index: usize,
+    list: &[(T, F)],
+) -> Option<(T, f64)> {
+    let get = |index: usize| {
+        let (t, f) = list.get(index)?;
+        Some((t.get(), f.get()))
+    };
+
+    let (independent_1, dependent_1) = get(index.checked_sub(1)?)?;
+    let (independent_2, dependent_2) = get(index)?;
+    let (independent_3, dependent_3) = get(index + 1)?;
+
+    let derivative_avg_12 = (dependent_2 - dependent_1) / (independent_2 - independent_1);
+    let derivative_avg_23 = (dependent_3 - dependent_2) / (independent_3 - independent_2);
+
+    let delta_independent_13 = independent_3 - independent_1;
+
+    Some((
+        T::new(independent_2.get()),
+        2.0 * (derivative_avg_23 - derivative_avg_12) / delta_independent_13,
+    ))
+}
+
+/// Calculates the numerical derivative of `F` with respect to `T` using time shifted data points.
+///
+/// Does not include the first or last data points.
+///
+/// Traditional "rise over run" derivatives calculate the average derivative, at the center of a
+/// time interval. This estimates the derivative at the _start_ of an interval.
+///
+/// For more, see William Leonard's "The Dangers of Automated Data Analysis," pub. _The Physics
+/// Teacher,_ vol. 35, April 1996, p. 220.
+///
+/// # Examples
+///
+/// Comparing with the actual second derivative:
+///
+/// ```rust
+/// # use sciutil::statistics::derivatives;
+/// #
+/// // `sin(t)` from `t = 0` to `t = 2`.
+/// let list = (0..=10)
+///     .map(|i| {
+///         let t = f64::from(i) * 0.2;
+///         (t, t.sin())
+///     })
+///     .collect::<Box<_>>();
+///
+/// // `-sin(t)` (because `d^2/dx^2 sin(t) = -sin(t)`) from `t = 0` to `t = 2`.
+/// let actual = list.iter().map(|(t, _)| (*t, -t.sin())).collect::<Box<_>>();
+/// assert_eq!(actual.len(), list.len());
+///
+/// // The numeric derivative.
+/// let result = derivatives::second_order_time_shifted(&list);
+/// assert_eq!(result.len(), actual.len() - 2);
+///
+/// // Your data may have more or less error than this!
+/// let accepted_error = 0.005;
+/// // Assert that the two values are within `accepted_error` of each other.
+/// let eq = |t, error, a: f64, b: f64| assert!((a - b).abs() <= error, "{a} != {b} @ {t}");
+///
+/// for i in 0..result.len() {
+///     let (t, derivative) = actual[i + 1];
+///     let (result_t, result_derivative) = result[i];
+///
+///     // The independent variables should remain unchanged.
+///     eq(t, 0.000_000_000_1, result_t, t);
+///     // The derivatives should be within `accepted_error`.
+///     eq(t, accepted_error, result_derivative, derivative);
+/// }
+/// ```
+///
+/// Comparing it to the values calculated by Vernier's Logger Pro®, version 3.16.2:
+///
+/// ```rust
+/// # use sciutil::statistics::derivatives;
+/// #
+/// const EXPECTED: [(f64, f64); 13] = [
+///     (0.2, -0.198_007_982_037),
+///     (0.4, -0.388_122_010_68),
+///     (0.6, -0.562_762_839_547),
+///     (0.8, -0.714_968_089_903),
+///     (1.0, -0.838_669_818_726),
+///     (1.2, -0.928_936_428_452),
+///     (1.4, -0.982_169_274_205),
+///     (1.6, -0.996_246_130_409),
+///     (1.8, -0.970_605_797_23),
+///     (2.0, -0.906_270_473_839),
+///     (2.2, -0.805_805_006_559),
+///     (2.4, -0.673_214_636_531),
+///     (2.6, -0.513_785_323_397),
+/// ];
+///
+/// // `sin(t)` from `t = 0` to `t = 2.8`.
+/// let list = (0..15)
+///     .map(|i| {
+///         let t = f64::from(i) * 0.2;
+///         (t, t.sin())
+///     })
+///     .collect::<Box<_>>();
+/// assert_eq!(list.len(), EXPECTED.len() + 2);
+///
+/// // The numeric derivative.
+/// let result = derivatives::second_order_time_shifted(&list);
+/// assert_eq!(result.len(), EXPECTED.len());
+///
+/// let effectively_equal = 0.000_000_000_1;
+/// // Assert that the two values are within `error` of each other.
+/// let eq = |t, error, a: f64, b: f64| assert!((a - b).abs() <= error, "{a} != {b} @ {t}");
+///
+/// for i in 0..result.len() {
+///     let (t, derivative) = EXPECTED[i];
+///     let (result_t, result_derivative) = result[i];
+///
+///     // The independent variables should remain unchanged.
+///     eq(t, effectively_equal, result_t, t);
+///     // The derivatives should be effectively equal.
+///     eq(t, effectively_equal, result_derivative, derivative);
+/// }
+/// ```
+#[must_use]
+#[expect(clippy::missing_panics_doc, reason = "see `expect` string")]
+pub fn second_order_time_shifted<T: Float, F: Float>(list: &[(T, F)]) -> Box<[(T, f64)]> {
+    if list.len() < 3 {
+        return Box::default();
+    }
+
+    let mut derivative = Vec::with_capacity(list.len() - 2);
+
+    // Skips the first and last index.
+    for i in 1..(list.len() - 1) {
+        derivative.push(
+            second_derivative_time_shifted(i, list)
+                .expect("`1 < i < list.len() - 1`, this is safe"),
         );
     }
 
